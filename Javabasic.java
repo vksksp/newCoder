1639ad06075cc4ed9f1137ef83834e1c654921cc
vksksp <102254228+vksksp@users.noreply.github.com>
2023-05-18 12:12:36 +0530

    Add files via upload

---
+++ Javabasic.java
@@ -0,0 +1,6 @@
+public class Javabasic {
+    public static void main(String args[]) {
+        int a = 10, b = 20;
+        System.out.println(a + b);
+    }
+}
---
+++ Typepromotion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Typepromotion {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        float b = sc.nextFloat();
+        char c = 'a';
+        System.out.println(a + b + c);
+
+    }
+}
